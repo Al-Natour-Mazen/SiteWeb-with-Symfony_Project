2470c5478259d4a39535f1fb53a703653ab65da0
Tom <caillaud.tom@outlook.fr>
2023-03-13 18:43:20 +0100

    Création de l'entité Produit avant migration

---
+++ src/Entity/Produit.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name:'i23_produits')]
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::STRING,length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(
+        name: 'prix_unitaire',      // inutile mais pour vous montrer qu'on a bien comprit
+        type: Types::FLOAT,
+        options: ['comment' => 'en euros'],
+    )]
+    private ?float $prixUnitaire = null;
+
+    #[ORM\Column(type: Types::INTEGER)]
+    private ?int $quantite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPrixUnitaire(): ?float
+    {
+        return $this->prixUnitaire;
+    }
+
+    public function setPrixUnitaire(float $prixUnitaire): self
+    {
+        $this->prixUnitaire = $prixUnitaire;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+}
